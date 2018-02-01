c96c350eecfbbe0474b32374e1734c2a70407fa5
Black-Greap <35989465+Black-Greap@users.noreply.github.com>
2018-02-01 22:54:49 +0545

    Added  a printcrap() function !

--- Practice/Practice/Source.cpp
+++ Practice/Practice/Source.cpp
@@ -2,7 +2,14 @@
 
 using std::cout;
 
+void print_Crap();
+
 int main() {
-	cout << "Something can't die yet !";
+	cout << "Something can't die yet ! \n";
+	print_Crap();
 	return 0;
+}
+
+void print_Crap() {
+	cout << "Watashi printing crap desu chan";
 }
